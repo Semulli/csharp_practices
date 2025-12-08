d29c565e0d621c0bf663dc2902c65d25148625f6
Semulli <semabdulazizova@gmail.com>
2025-12-08 09:31:09 +0400

    last changes added

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -6,42 +6,8 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
-            int[] numbers = new int[10];
-            int evenNumsSum = 0;
-            int evenNums = 0;
-            int oddNums = 0;
-            int oddNumsSum = 0;
-
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.WriteLine($"please enter ${i}. index of array");
-                int yourChoice = int.Parse(Console.ReadLine());
-
-                numbers[i] = yourChoice;
-            }
-
-            for (int j = 0; j < numbers.Length; j++)
-            {
-                if (numbers[j] % 2 == 0)
-                {
-                    evenNums++;
-                    evenNumsSum += numbers[j];
-                }
-                else
-                {
-                    oddNums++;
-                    oddNumsSum += numbers[j];
-                }
-            }
-
-            Console.WriteLine("***********************************");
-
-            Console.WriteLine($"even numbers sum: ${evenNumsSum}");
-            Console.WriteLine($"even numbers count: ${evenNums}");
-            Console.WriteLine("***********************************");
-            Console.WriteLine($"odd numbers sum: ${oddNumsSum}");
-            Console.WriteLine($"odd numbers count: ${oddNums}");
+            car myCar = new car();
+            myCar.carModel = "";
         }
     }
 }
---
+++ MyApp/MyApp/car.cs
@@ -0,0 +1,19 @@
+namespace MyApp;
+
+public class car
+{
+    public int doorCount;
+    public string carModel;
+    public string carColor;
+
+
+    public void runMotor()
+    {
+        Console.WriteLine("the car is running");
+    }
+
+    public void lockDoors()
+    {
+        Console.WriteLine("the door is locked.....");
+    }
+}
